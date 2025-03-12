15c360b7cb2fc34da38162ee312bf8e139268561
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-03-12 19:43:46 +0100

    feat(homepage): darkmode + responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,43 @@
 import styles from "./page.module.css";
-import Stars from "../..//public/assets/stars.svg";
-import Logo from "../..//public/assets/logo.svg";
+import Stars from "../../public/assets/stars.svg";
 import Image from "next/image";
 import Button from "./components/Button";
 import { QrCode } from "lucide-react";
+import PapillonLabsLogo from "./components/PapillonLabLogo";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <>
-          <Image src={Stars} alt={""} style={{
-            position: "absolute",
-            top: 0,
-            right: 0
-          }}/>
+          <Image
+            src={Stars}
+            alt={""}
+            className={styles.stars}
+          />
 
-          <div style={{
-            display: "flex",
-            width: 670,
-            padding: "28px 46px",
-            backgroundColor: "#FFFFFF",
-            gap: "30px",
-            flexDirection: "column",
-            alignItems: "center",
-            borderRadius: 22,
-            border: "1px solid rgba(0, 0, 0, 0.15)"
-          }}>
-            <Image src={Logo} alt={"Papillon Labs Logo"}/>
-            <span style={{
-              alignSelf: "stretch",
-              color: "#000",
-              textAlign: "center",
-              fontFamily: "Fixel Text",
-              fontSize: "17px",
-              fontStyle: "normal",
-              opacity: 0.55,
-              fontWeight: 600
-            }}>Bienvenue sur Papillon Labs ! Exportez vos données scolaires et contribuez à l’amélioration de Papillon Magic+. Connectez-vous avec votre compte scolaire pour continuer.</span>
-            <div style={{
-              display: "flex",
-              flexDirection: "column",
-              width: "100%"
-            }}>
+          <div className={styles.container}>
+            <PapillonLabsLogo />
+            <span className={styles.introText}>
+              Bienvenue sur Papillon Labs ! Exportez vos données scolaires et
+              contribuez à l’amélioration de Papillon Magic+. Connectez-vous avec
+              votre compte scolaire pour continuer.
+            </span>
+            <div className={styles.buttons}>
               <Button centered>
                 <p>Se connecter via mes identifiants</p>
               </Button>
-              <Button leading={<QrCode absoluteStrokeWidth={true} size={20}/>} variant="secondary" centered>
+              <Button
+                leading={<QrCode absoluteStrokeWidth={true} size={20} />}
+                variant="secondary"
+                centered
+              >
                 <p>Utiliser un QR-Code</p>
               </Button>
             </div>
-            <span style={{
-              alignSelf: "stretch",
-              color: "#000",
-              textAlign: "center",
-              fontFamily: "Fixel Text",
-              fontSize: "14px",
-              fontStyle: "normal",
-              opacity: 0.55,
-              fontWeight: 500
-            }}>Pour l’instant, seuls les comptes PRONOTE sont pris en charge.</span>
+            <span className={styles.subText}>
+              Pour l’instant, seuls les comptes PRONOTE sont pris en charge.
+            </span>
           </div>
         </>
       </main>
