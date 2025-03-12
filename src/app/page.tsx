c12b8f1bdf7b02e3660f3866be92403e0ef4c233
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-03-12 18:11:35 +0100

    fix: disabled background for primary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { QrCode, TagIcon } from "lucide-react";
+import { Calendar, ChartPie, Image, MoreHorizontal, QrCode, School, User } from "lucide-react";
 import Button from "./components/Button";
 import styles from "./page.module.css";
 import InformativeBox from "./components/InformativeBox";
@@ -8,14 +8,26 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <div style={{ width: 400, display: "flex", flexDirection: "column", gap: 10}}>
-          <Button centered>
+          <Button centered disabled>
             <p>Se connecter via mes identifiants</p>
           </Button>
-          <Button leading={<QrCode absoluteStrokeWidth={true} size={20}/>} variant="secondary" centered>
+          <Button leading={<QrCode absoluteStrokeWidth={true} size={20}/>} variant="secondary" centered disabled>
             <p>Utiliser un QR-Code</p>
           </Button>
-
-          <InformativeBox icon={TagIcon}>Ton nom, prénom et classe</InformativeBox>
+        </div>
+        <div style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: 10,
+          maxWidth: 600,
+          flexWrap: "wrap"
+        }}>
+          <InformativeBox icon={User}>Ton nom, prénom et classe</InformativeBox>
+          <InformativeBox icon={Image}>Ta photo de classe</InformativeBox>
+          <InformativeBox icon={Calendar}>Ton emploi du temps</InformativeBox>
+          <InformativeBox icon={ChartPie}>Tes notes et moyennes</InformativeBox>
+          <InformativeBox icon={School}>Tes absences et retard</InformativeBox>
+          <InformativeBox icon={MoreHorizontal}>Données diverses</InformativeBox>
         </div>
       </main>
     </div>
