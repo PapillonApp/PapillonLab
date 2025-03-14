09a635576973a0a247ed2c41b7d32f79699a66fe
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-03-14 02:04:30 +0100

    fix: darkmode

--- src/app/components/ClassName.tsx
+++ src/app/components/ClassName.tsx
@@ -1,24 +1,13 @@
 import React from "react";
-
+import styles from "./ClassName.module.css";
 
 const ClassName: React.FC<{ text: string }> = ({ text }) => {
     return (
         <div
-          style={{
-            backgroundColor: "#006DB929",
-            borderRadius: 8,
-            padding: "4px 6px",
-            overflow: "hidden",
-          }}
+          className={styles.container}
         >
           <span
-            style={{
-              color: "#006DB9",
-              fontSize: 16,
-              letterSpacing: 0.5,
-              margin: 0,
-              marginBottom: 2,
-            }}
+            className={styles.text}
           >{text}</span>
         </div>
     );
