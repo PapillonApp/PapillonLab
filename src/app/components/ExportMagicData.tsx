fdaef62e57717f20ad03d8d7b9251bb3c7f1fe40
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-03-17 22:31:40 +0100

    fix(export-magic): enhance style

--- src/app/components/ExportMagicData.tsx
+++ src/app/components/ExportMagicData.tsx
@@ -1,7 +1,5 @@
-import { LucideIcon, MicVocal, Share, Sparkles, TrafficCone } from "lucide-react";
+import { LucideIcon, MicVocal, Sparkles, TrafficCone } from "lucide-react";
 import styles from "./ExportData.module.css";
-import InformativeBox from "./InformativeBox";
-import Button from "./Button";
 import React from "react";
 import { exportMagicDataset } from "../func/ExportMagicData";
 import PapillonSteps from "./PapillonSteps";
@@ -13,10 +11,10 @@ interface DataType {
 }
 
 interface ExportMagicDataProps {
-    collectedDatas: Array<DataType>
+    collectedDatas?: Array<DataType>
 }
 
-const ExportMagicData: React.FC<ExportMagicDataProps> = ({ collectedDatas }) => {
+const ExportMagicData: React.FC<ExportMagicDataProps> = () => {
     const [exporting, setExporting] = React.useState<boolean>(false);
     const [exportingStep, setExportingStep] = React.useState<number>(0);
 
@@ -27,29 +25,30 @@ const ExportMagicData: React.FC<ExportMagicDataProps> = ({ collectedDatas }) => 
     }
 
     return (
-        <div className={styles.container }>
+        <div className={styles.container} style={{ width: "100%", height: "100%", padding: 30, alignItems: "center", justifyContent: "center", display: "flex", position: "relative"}}>
+            <div style={{position: "absolute", top: 20, left: 20, width: "100%"}}>
+                    <div className={styles.title}>
+                        <Sparkles />
+                        Papillon Magic+
+                    </div>
+                    <div className={styles.subtitle}>
+                            Vérifiez attentivement les résultats détectés par le modèle actuel.
+                    </div>
+                </div>
             <div style={{display: "flex", flexDirection: "column", gap: 10, position: "absolute"}} className={`${exporting ? styles.slideout : ""}`}>
-                <div className={styles.title}>
-                    <Share />
-                    Papillon Magic+
-                </div>
-                <div className={styles.subtitle}>
-                    Exporte un fichier pour entraîner PapillonMagic+, notre modèle IA avancé. Tes données contribueront directement à son amélioration et ne transiteront jamais par un serveur tiers.
-                </div>
-                <span className={`${styles.subtitle} ${styles.exportedDataText}`} style={{ marginTop: 10 }}>Tes données exportées incluront :</span>
-                <div className={styles.datasContainer}>
+                <div style={{display: "flex", alignItems: "center", justifyContent: "center", marginTop: 30, flexDirection: "column", gap: 10}}>
+                    <div className={styles.homework}>
+                        Réaliser un exposé sur le thème: Papillon Magic+
+                    </div>
                     <MagicButton icon={Sparkles} label="Évaluation" color="FF9514"/>
-                    <MagicButton icon={MicVocal} label="Devoir Maison" color="BD14FF" keybind="D"/>
-                    <MagicButton icon={MicVocal} label="Compte Rendu" color="CE5347" keybind="F"/>
-                    <MagicButton icon={MicVocal} label="Présentations Orales" color="145FFF" keybind="G"/>
-                    <MagicButton icon={MicVocal} label="Lecture" color="13c35d" keybind="H"/>
-                    <MagicButton icon={MicVocal} label="Aucun" color="AFAFAF" keybind="Suppr"/>
-                    {collectedDatas.map((data, index) => (
-                        <InformativeBox key={index} icon={data.icon}>{data.label}</InformativeBox>
-                    ))}
-                </div>
-                <div style={{paddingTop: 10}}>
-                    <Button centered withShadow onPress={handleBtnPress}><span style={{height: 40, alignItems: "center", display: "flex"}}>Exporter mes données</span></Button>
+                    <span className={styles.subtitle} style={{ width: "fit-content", marginTop: "10px" }}>Si le résultat est incorrect, sélectionne la catégorie appropriée.</span>
+                    <div className={styles.datasContainer}>
+                        <MagicButton icon={MicVocal} label="Devoir Maison" color="BD14FF" keybind="D"/>
+                        <MagicButton icon={MicVocal} label="Compte Rendu" color="CE5347" keybind="F"/>
+                        <MagicButton icon={MicVocal} label="Présentations Orales" color="145FFF" keybind="G"/>
+                        <MagicButton icon={MicVocal} label="Lecture" color="13c35d" keybind="H"/>
+                        <MagicButton icon={MicVocal} label="Aucun" color="AFAFAF" keybind="Suppr"/>
+                    </div>
                 </div>
             </div>
             <div style={{display: "flex", flexDirection: "column", gap: 10, transform: "translateX(1000%)"}} className={`${exporting ? styles.slidein : ""}`}>
